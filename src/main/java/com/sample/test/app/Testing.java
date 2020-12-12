18ffd31b762e9911363e4fb4e52b12a0b33d3b24
Radhakrishnan <416647@cognizant.com>
2020-12-12 19:47:06 +0530

    added fix

--- src/main/java/com/sample/test/app/Testing.java
+++ src/main/java/com/sample/test/app/Testing.java
@@ -4,13 +4,9 @@ public class Testing {
 
 	public Model verifyTest() {
 		Model request = null;
-		try {			
-			request.setClassName("com.cts.selfhealer.config.SelfHealerUIServer");
-			request.setMethodName("methodName");
-			System.out.println("verifyTest : " + request);
-		} catch (RuntimeException e) {
-			System.out.println("Null pointer " + e.getMessage());
-		}
+		request.setClassName("com.cts.selfhealer.config.SelfHealerUIServer");
+		request.setMethodName("methodName");
+		System.out.println("verifyTest : " + request);
 		return request;
 	}
 	
